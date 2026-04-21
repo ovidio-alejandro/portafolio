49c0b785fcda8a976fdc0c8be41fc6b3e6636ce4
ovidio-alejandro <276051398+ovidio-alejandro@users.noreply.github.com.>
2026-04-22 00:38:30 +0200

    arreglando un error en el archivo Projects.js

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -4,11 +4,11 @@ function Projects() {
   return (
     <section className="projects" id="projects">
 
-      <h2 className="projects-title">Mis Proyectos</h2>
+     { /*<h2 className="projects-title">Mis Proyectos</h2>
 
       <div className="projects-container">
 
-        {/* Proyecto 1 */}
+        
         <div className="project-card">
           <h3>Proyecto 1</h3>
           <p>Descripción breve del proyecto que hiciste.</p>
@@ -19,7 +19,7 @@ function Projects() {
           </div>
         </div>
 
-        {/* Proyecto 2 */}
+        
         <div className="project-card">
           <h3>Proyecto 2</h3>
           <p>Descripción breve del proyecto que hiciste.</p>
@@ -30,7 +30,7 @@ function Projects() {
           </div>
         </div>
 
-        {/* Proyecto 3 */}
+        
         <div className="project-card">
           <h3>Proyecto 3</h3>
           <p>Descripción breve del proyecto que hiciste.</p>
@@ -41,7 +41,7 @@ function Projects() {
           </div>
         </div>
 
-      </div>
+      </div>*/}
 
     </section>
   );
