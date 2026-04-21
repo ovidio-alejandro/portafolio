a676c8ea3030e835b6335aab2657d25e8b356849
ovidio-alejandro <276051398+ovidio-alejandro@users.noreply.github.com.>
2026-04-21 23:46:59 +0200

    se modifico la direccion de envio del formulario a whatsap

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -1,6 +1,31 @@
 import "./Contact.css";
 
 function Contact() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const nombre = e.target.nombre.value;
+    const email = e.target.email.value;
+    const mensaje = e.target.mensaje.value;
+
+    const telefono = "5358553329";
+
+    const texto = `
+Hola 👋
+
+Mi nombre es: ${nombre}
+Mi email: ${email}
+
+Mensaje:
+${mensaje}
+    `;
+
+    const url = `https://wa.me/${telefono}?text=${encodeURIComponent(texto)}`;
+
+    window.open(url, "_blank");
+  };
+
   return (
     <section className="contact" id="contact">
 
@@ -12,28 +37,34 @@ function Contact() {
           ¿Tienes un proyecto o quieres hablar? Escríbeme 👇
         </p>
 
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={handleSubmit}>
 
           <input
             type="text"
+            name="nombre"
             placeholder="Tu nombre"
             className="input"
+            required
           />
 
           <input
             type="email"
+            name="email"
             placeholder="Tu email"
             className="input"
+            required
           />
 
           <textarea
+            name="mensaje"
             placeholder="Tu mensaje"
             className="textarea"
             rows="5"
+            required
           ></textarea>
 
           <button type="submit" className="button">
-            Enviar mensaje
+            Enviar por WhatsApp
           </button>
 
         </form>
